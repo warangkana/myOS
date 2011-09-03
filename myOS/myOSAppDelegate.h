41eea2c8eaf198df8e8e90f33a048ef2fa5bd007
warangkana <warangkana.tej@gmail.com>
2011-09-03 11:22:48 +0700

    Initial Commit

---
+++ myOS/myOSAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  myOSAppDelegate.h
+//  myOS
+//
+//  Created by Warangkana Tejasakulsin on 9/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myOSAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
